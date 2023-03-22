1917f5f38addb138c1396b00518a59e5db1b8702
Fedor <fedorinter2004@gmail.com>
2023-03-22 23:39:21 +0300

    last

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv){
   string name;                                      // Name of @user
   std::cin >> name;                                 // Input name of @user
   std::cout << "Hello world from " << name << endl; // Output name of @user
